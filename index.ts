be378643058166f6e91a375db55ce9e55e4ebada
jjong <akrTmwk@gmail.com>
2023-05-05 11:50:47 +0900

    feat: content 스팸여부 함수 추가

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 export function isSpam(
   content: string, spamLinkDomains: string[], redirectionDepth: number
 ): boolean {
-  // TODO content 안에 spamLinkDomains 가 하나라도 포함되어있으면 true 를 리턴한다.
+  for (let i = 1; i <= redirectionDepth; i++) {
+    if (isSpamContent(content, spamLinkDomains)) {
+      return true;
+    }
+  }
 
-  /*
-  TODO 없으면 redirectionDepth 만큼 리다이렉트하는동안 (* 내용 확인 필요)
-  리다이렉트 된 페이지 내 content 중 spamLinkDomains 가 하나라도 포함되어있으면 true 를 리턴한다.
-   */
+  return false;
+}
 
-  return true;
+function isSpamContent(content: string, spamLinkDomains: string[]): boolean {
+  return spamLinkDomains.some((spamLinkDomain) => {
+    return content.includes(spamLinkDomain);
+  });
 }
