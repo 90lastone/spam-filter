bd253efe17962d484ebe7a6d774c4369f50d06f2
jjong <akrTmwk@gmail.com>
2023-05-05 11:50:54 +0900

    test: content 스팸여부 함수 추가

--- index.test.ts
+++ index.test.ts
@@ -1,11 +1,40 @@
 import { isSpam } from "./index";
 
 describe("isSpam", () => {
+  const content = "spam spam https://moiming.page.link/exam?_imcp-1";
+
   test("test1", () => {
-    const content = "spam spam https://moiming.page.link/exam?_imcp-1";
+    const spamLinkDomains = ["docs.github.com"];
+    const redirectionDepth = 1;
+
+    expect(isSpam(content, spamLinkDomains, redirectionDepth)).toBe(false);
+  })
+
+  test("test2", () => {
     const spamLinkDomains = ["moiming.page.link"];
     const redirectionDepth = 1;
 
     expect(isSpam(content, spamLinkDomains, redirectionDepth)).toBe(true);
   })
+
+  // test("test3", () => {
+  //   const spamLinkDomains = ["github.com"];
+  //   const redirectionDepth = 2;
+  //
+  //   expect(isSpam(content, spamLinkDomains, redirectionDepth)).toBe(true);
+  // })
+  //
+  // test("test4", () => {
+  //   const spamLinkDomains = ["docs.github.com"];
+  //   const redirectionDepth = 2;
+  //
+  //   expect(isSpam(content, spamLinkDomains, redirectionDepth)).toBe(false);
+  // })
+  //
+  // test("test5", () => {
+  //   const spamLinkDomains = ["docs.github.com"];
+  //   const redirectionDepth = 3;
+  //
+  //   expect(isSpam(content, spamLinkDomains, redirectionDepth)).toBe(true);
+  // })
 });
