bd100820b7d079f9f204051c1d7b8fc8ccf8255d
jjong <akrTmwk@gmail.com>
2023-05-05 11:29:36 +0900

    docs: 작업내용 간단 메모

--- index.ts
+++ index.ts
@@ -1,5 +1,12 @@
 export function isSpam(
   content: string, spamLinkDomains: string[], redirectionDepth: number
 ): boolean {
+  // TODO content 안에 spamLinkDomains 가 하나라도 포함되어있으면 true 를 리턴한다.
+
+  /*
+  TODO 없으면 redirectionDepth 만큼 리다이렉트하는동안 (* 내용 확인 필요)
+  리다이렉트 된 페이지 내 content 중 spamLinkDomains 가 하나라도 포함되어있으면 true 를 리턴한다.
+   */
+
   return true;
 }
